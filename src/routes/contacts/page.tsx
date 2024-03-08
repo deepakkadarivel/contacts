3e632ca137fef5913b02ac6cf3c6ec5e226d76e6
Deepak <d.kadarivel@relay42.com>
2024-03-08 23:27:42 +0530

    Setup Dark & Light mode theming

--- src/routes/contacts/page.tsx
+++ src/routes/contacts/page.tsx
@@ -1,5 +1,26 @@
+import MainNav from "@/components/main-nav";
+import { Search } from "@/components/search";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+
 function Contacts() {
-  return <div>Contacts</div>;
+  return (
+    <div className="hidden flex-col md:flex">
+      <div className="border-b">
+        <div className="flex h-16 items-center px-4">
+          <Button>Logo</Button>
+          <MainNav className="mx-6" />
+          <div className="ml-auto flex items-center space-x-4">
+            <Search />
+            <Avatar className="h-8 w-8">
+              <AvatarImage src="/avatars/01.png" alt="@shadcn" />
+              <AvatarFallback>SC</AvatarFallback>
+            </Avatar>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Contacts;
--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,14 +1,17 @@
+import { ModeToggle } from "@/components/mode-toggle";
+import { ThemeProvider } from "@/components/theme-provider";
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
 
 function Root() {
   return (
-    <div>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <p>Vite + React router</p>
+      <ModeToggle />
       <Button asChild>
         <Link to="/contacts">Contacts</Link>
       </Button>
-    </div>
+    </ThemeProvider>
   );
 }
 
